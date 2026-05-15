94c93ee4e2c2df7f9ee468778fbe077be2b5cd8c
AfanRiaz <afanriaz202@gmail.com>
2026-05-15 13:00:47 +0500

    task 1 completed

--- task_1/image_optimization_app/lib/main.dart
+++ task_1/image_optimization_app/lib/main.dart
@@ -1,7 +1,11 @@
-import 'dart:io';
+import 'dart:convert';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +13,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,7 +35,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<File> images = [];
+  List<String> images = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,18 +48,30 @@ class _HomePageState extends State<HomePage> {
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
               crossAxisSpacing: 10,
-              mainAxisSpacing: 10
+              mainAxisSpacing: 10,
             ),
             itemCount: images.length,
+            physics: BouncingScrollPhysics(),
             itemBuilder: (context, index){
               return ClipRRect(
                   borderRadius: BorderRadius.circular(15),
-                child: Image.file(images[index],fit: BoxFit.cover,),
+                child: CachedNetworkImage(
+                  imageUrl: images[index],fit: BoxFit.cover,
+                  placeholder: (context, images){
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                  errorWidget: (context, images, error){
+                    return Icon(Icons.error_outline);
+                  },
+                ),
               );
             }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
+          //Pick Image from gallery
           final picker = ImagePicker();
           final XFile? pickedImage = await picker.pickImage(
             source: ImageSource.gallery,
@@ -64,21 +79,47 @@ class _HomePageState extends State<HomePage> {
           if(pickedImage !=null){
             compressFile(pickedImage.path);
           }
-
         },
         child: const Icon(Icons.image, size: 40),
       ),
     );
   }
+  //compress image
   Future<void> compressFile(String path) async{
     final compressedImag = await FlutterImageCompress.compressAndGetFile(
       path, '${path}_comp.jpg',
       quality: 60,
     );
     if(compressedImag != null) {
+       await uploadToImgBB(compressedImag.path);
+    }
+  }
+  Future<void> uploadToImgBB(String imagePath) async{
+    const String apiKey = '213b33cf8401e1c6d240cc08e8615692';
+    final url = Uri.parse('https://api.imgbb.com/1/upload?key=$apiKey',);
+    final request = http.MultipartRequest(
+      'POST',
+      url,
+    );
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'image',
+        imagePath
+      )
+    );
+    final response = await request.send();
+
+    if(response.statusCode == 200){
+      final responseData = await response.stream.bytesToString();
+      final jsonData = jsonDecode(responseData);
+      final imageUrl = jsonData['data']['display_url'];
       setState(() {
-        images.add(File(compressedImag.path));
+        images.add(imageUrl);
       });
+      Fluttertoast.showToast(msg: imageUrl);
+    }
+    else{
+      Fluttertoast.showToast(msg: 'Error Occurred');
     }
   }
 }
