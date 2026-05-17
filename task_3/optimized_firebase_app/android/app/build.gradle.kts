768783ff6197d0dd8f04c7b7c813b6c269721797
AfanRiaz <afanriaz202@gmail.com>
2026-05-17 10:15:08 +0500

    task 4 commiting

--- task_3/optimized_firebase_app/android/app/build.gradle.kts
+++ task_3/optimized_firebase_app/android/app/build.gradle.kts
@@ -1,27 +1,26 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
+
+    // Firebase
     id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
+
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
 dependencies {
-    // Import the Firebase BoM
+
+    // Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
 
-
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
+    // Firebase Analytics
     implementation("com.google.firebase:firebase-analytics")
-
-
-    // Add the dependencies for any other desired Firebase products
-    // https://firebase.google.com/docs/android/setup#available-libraries
 }
+
 android {
+
     namespace = "com.example.optimized_firebase_app"
+
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -35,20 +34,32 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
         applicationId = "com.example.optimized_firebase_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
+
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+
+        getByName("release") {
+
+            // APK Optimization
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            // ProGuard
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- task_3/optimized_firebase_app/lib/screens/home_screen.dart
+++ task_3/optimized_firebase_app/lib/screens/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../models/product_model.dart';
-import '../services/firestore_service.dart';
-import '../widgets/product_tile.dart';
+// Deferred Import
+import 'profile_screen.dart' deferred as profile;
 
 class HomeScreen extends StatefulWidget {
+
   const HomeScreen({super.key});
 
   @override
@@ -12,53 +12,34 @@ class HomeScreen extends StatefulWidget {
       _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-
-  final FirestoreService firestoreService =
-  FirestoreService();
-
-  List<ProductModel> products = [];
+class _HomeScreenState
+    extends State<HomeScreen> {
 
   bool isLoading = false;
 
-  @override
-  void initState() {
-    super.initState();
-
-    loadProducts();
-  }
-
   // =========================
-  // LOAD PRODUCTS
+  // LOAD PROFILE FEATURE
   // =========================
 
-  Future<void> loadProducts() async {
+  Future<void> openProfile() async {
 
     setState(() {
       isLoading = true;
     });
 
-    List<ProductModel> fetchedProducts =
-    await firestoreService.fetchProducts();
-
-    products.addAll(fetchedProducts);
+    // Lazy Load Screen
+    await profile.loadLibrary();
 
     setState(() {
       isLoading = false;
     });
-  }
 
-  // =========================
-  // INCREMENT VIEWS
-  // =========================
-
-  Future<void> increaseViews(String id) async {
-
-    await firestoreService.incrementViews(id);
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text("Views Increased"),
+    // Navigate
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) =>
+        const profile.ProfileScreen(),
       ),
     );
   }
@@ -70,63 +51,25 @@ class _HomeScreenState extends State<HomeScreen> {
 
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Optimized Firestore App"),
+        title: const Text(
+          "Deferred Loading App",
+        ),
       ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
+      body: Center(
 
-          await firestoreService.batchInsert();
+        child: isLoading
 
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text("Batch Insert Success"),
-            ),
-          );
-        },
+            ? const CircularProgressIndicator()
 
-        child: const Icon(Icons.add),
-      ),
+            : ElevatedButton(
 
-      body: Column(
-        children: [
+          onPressed: openProfile,
 
-          Expanded(
-            child: ListView.builder(
-              itemCount: products.length,
-
-              itemBuilder: (context, index) {
-
-                return ProductTile(
-
-                  product: products[index],
-
-                  onTap: () async {
-
-                    setState(() {
-                    });
-                    await increaseViews(
-                      products[index].id,
-                    );
-                  },
-                );
-              },
-            ),
+          child: const Text(
+            "Open Profile Screen",
           ),
-
-          if (isLoading)
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: CircularProgressIndicator(),
-            ),
-
-          ElevatedButton(
-            onPressed: loadProducts,
-            child: const Text("Load More"),
-          ),
-
-          const SizedBox(height: 20),
-        ],
+        ),
       ),
     );
   }
